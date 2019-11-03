aba09c48c59dcfa64d7b3f308b4cc523252e1e98
Nainoa <Nainoaian@gmail.com>
2019-11-02 20:16:56 -1000

    Round 1 Commit
    Landing Page mockup finished in 22:14 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+          <div className="digits-landing-background">
+          <Grid stackable centered container columns={3}>
+            <Grid.Column textAlign='centered'>
+              <Icon size='massive' name='users' inverted />
+              <Header as='h1' inverted>Multiple Users</Header>
+              <Header as='h3' inverted>
+                This address book enables any number of users to register
+                and save their business contacts. You can only see the contacts
+                you have created.
+              </Header>
+            </Grid.Column>
+            <Grid.Column textAlign='centered'>
+              <Icon size='massive' name='file alternate' inverted />
+              <Header as='h1' inverted>Contact Info</Header>
+              <Header as='h3' inverted>
+                For each contact, you can save their number, address, and phone number.
+              </Header>
+            </Grid.Column>
+            <Grid.Column textAlign='centered'>
+              <Icon size='massive' name='calendar check outline' inverted />
+              <Header as='h1' inverted>Timestamped Notes</Header>
+              <Header as='h3' inverted>
+                Each time you make contact with a contact, you can write a note that
+                summarizes the conversation. This note is saved along with a timestamp
+                with the contact.
+              </Header>
+            </Grid.Column>
+          </Grid>
+          </div>
     );
   }
 }
